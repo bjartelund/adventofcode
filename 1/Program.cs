86b263f9b57cdf754c5c78e5c755f2be717c1450
Bjarte Aarmo Lund <bjarte.lund@uit.no>
2021-12-03 20:40:22 +0100

    Finished day 1 part 2 start day 2

--- 1/Program.cs
+++ 1/Program.cs
@@ -6,19 +6,26 @@ class IncreasingValues
 	static void Main()
 	{
 		string[] lines = System.IO.File.ReadAllLines(@"input");
-		int prev = -1;
-		int increases=0;
+		int prev2 = -1;
+		int prev1 = -1;
+		int prevsum = 0;
+		int increases=-1;
+		int sum=0;
 		foreach (string line in lines)
 		{
 			Console.WriteLine("\t" + line);
 			int current = Int32.Parse(line);
-			if (prev < current && prev != -1)
+			
+			sum= current+prev1+prev2;
+			Console.WriteLine(prevsum);
+			if (sum > current && prevsum < sum && prev2 != -1)
 			{
 				Console.WriteLine("Increase");
 				increases++;
 			}
-
-			prev=current;
+			prev2=prev1;
+			prev1=current;
+			prevsum=sum;
 		}
 		Console.WriteLine($"The number of increases is {increases}");
 

263b0ecbd153df62c178a501bfbbd125975351cb
Bjarte Aarmo Lund <bjarte.lund@uit.no>
2021-12-03 20:26:08 +0100

    Day 1, Part 1 working

---
+++ 1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class IncreasingValues
+{
+	static void Main()
+	{
+		string[] lines = System.IO.File.ReadAllLines(@"input");
+		int prev = -1;
+		int increases=0;
+		foreach (string line in lines)
+		{
+			Console.WriteLine("\t" + line);
+			int current = Int32.Parse(line);
+			if (prev < current && prev != -1)
+			{
+				Console.WriteLine("Increase");
+				increases++;
+			}
+
+			prev=current;
+		}
+		Console.WriteLine($"The number of increases is {increases}");
+
+	}
+
+}

8945723930486739fecb48e75360b85a3eafa030
Bjarte Aarmo Lund <bjarte.lund@uit.no>
2021-12-06 19:54:24 +0100

    Solution to day 6, part 1. Still issue with part 2. Partial solution to day 3.

--- 3/Program.cs
+++ 3/Program.cs
@@ -4,6 +4,22 @@ using System.Collections.Generic;
 
 class IncreasingValues
 {
+	static string[] reduce(string[] input, int pos, int value)
+	{
+		string[] output= new string[input.Length];
+		int i=0;
+		foreach (string inputvalue in input)
+		{
+			if ( inputvalue.Length >= pos && Int32.Parse(inputvalue[pos].ToString()) == value)
+			{
+				output[i++]=inputvalue;
+			}
+			else
+				output[i++]="";
+		}
+		return output;
+	}
+
 	static void Main()
 	{
 		string[] lines = System.IO.File.ReadAllLines(@"input");
@@ -24,12 +40,12 @@ class IncreasingValues
 		string epsilonbinary="";
 		foreach (int count in counts)
 		{
-			if (count < lines.Length/2)
+			if (count <= lines.Length/2)
 			{
 				gammabinary+="0";	
 				epsilonbinary+="1";	
 			}
-			else if (count > lines.Length/2)
+			else if (count >= lines.Length/2)
 			{
 				gammabinary+="1";	
 				epsilonbinary+="0";	
@@ -42,6 +58,58 @@ class IncreasingValues
 		Console.WriteLine(gammadecimal);
 		Console.WriteLine(epsilondecimal);
 		Console.WriteLine(Int32.Parse(gammadecimal)*Int32.Parse(epsilondecimal));
+		int j = 0;
+		string[] reduced= new string[lines.Length];
+		Array.Copy(lines,reduced,lines.Length);
+		foreach (char bit in gammabinary)
+		{
+			if (bit.ToString()!="")
+			{
+				reduced=reduce(reduced,j++,Int32.Parse(bit.ToString()));
+			}
+			int k=0;
+			foreach (string number in reduced)
+				
+				if (number!="")
+				{
+					k++;
+				}
+			if (k==2)
+				break;
+		}
+		Console.WriteLine(reduced.Max());
+		string oxygengeneratorrating=Convert.ToInt32(reduced.Max(),2).ToString();
+		Console.WriteLine(oxygengeneratorrating);
+		Array.Copy(lines,reduced,lines.Length);
+		j=0;
+			
+		foreach (char bit in epsilonbinary)
+		{
+			if (bit.ToString()!="")
+			{
+				reduced=reduce(reduced,j++,Int32.Parse(bit.ToString()));
+				//Console.WriteLine(bit.ToString());
+			}
+			int k=0;
+
+			foreach (string number in reduced)
+			{	
+				if (number!="")
+				{
+					k++;
+					//Console.WriteLine(number);
+				}
+			if (k>=2)
+				break;
+			Console.WriteLine(k);
+			}
+		}
+		reduced= reduced.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+		Console.WriteLine(reduced.Min());
+		string co2scrubber=Convert.ToInt32(reduced.Min(),2).ToString();
+		Console.WriteLine(co2scrubber);
+		Console.WriteLine(Int32.Parse(oxygengeneratorrating)*Int32.Parse(co2scrubber));
+		
 
 
 	}
---
+++ 6/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = "3,4,3,1,2";
+//string input = "1,1,1,3,3,2,1,1,1,1,1,4,4,1,4,1,4,1,1,4,1,1,1,3,3,2,3,1,2,1,1,1,1,1,1,1,3,4,1,1,4,3,1,2,3,1,1,1,5,2,1,1,1,1,2,1,2,5,2,2,1,1,1,3,1,1,1,4,1,1,1,1,1,3,3,2,1,1,3,1,4,1,2,1,5,1,4,2,1,1,5,1,1,1,1,4,3,1,3,2,1,4,1,1,2,1,4,4,5,1,3,1,1,1,1,2,1,4,4,1,1,1,3,1,5,1,1,1,1,1,3,2,5,1,5,4,1,4,1,3,5,1,2,5,4,3,3,2,4,1,5,1,1,2,4,1,1,1,1,2,4,1,2,5,1,4,1,4,2,5,4,1,1,2,2,4,1,5,1,4,3,3,2,3,1,2,3,1,4,1,1,1,3,5,1,1,1,3,5,1,1,4,1,4,4,1,3,1,1,1,2,3,3,2,5,1,2,1,1,2,2,1,3,4,1,3,5,1,3,4,3,5,1,1,5,1,3,3,2,1,5,1,1,3,1,1,3,1,2,1,3,2,5,1,3,1,1,3,5,1,1,1,1,2,1,2,4,4,4,2,2,3,1,5,1,2,1,3,3,3,4,1,1,5,1,3,2,4,1,5,5,1,4,4,1,4,4,1,1,2";
+string[] inputstrarray= input.Split(",");
+int[] inputarray= new int[inputstrarray.Length];
+int i=0;
+
+foreach (string inputnumber in inputstrarray)
+	inputarray[i++]=Int32.Parse(inputnumber);
+
+
+static int[] iterate(int[] inputarray)
+{
+	int spawn=0;
+	int i=0;
+	foreach (int number in inputarray)
+	{
+		if (number == 0)
+			spawn++;
+	}
+	int[] outputarray= new int[inputarray.Length+spawn];
+	foreach (int number in inputarray)
+	{
+		if (number <= 6)
+			outputarray[i++] = ((number-1 % 7)+7)%7;
+		else
+			outputarray[i++] = number - 1;
+	}
+	while (spawn >= 1)
+	{
+		outputarray[i++]=8;
+		spawn--;
+	}
+	return outputarray;
+}
+i=1;
+int[] outputarray= iterate(inputarray);
+while (i !=150)
+{
+	outputarray= iterate(outputarray);
+	//Console.WriteLine(String.Join(",",outputarray));
+i++;
+}
+Console.WriteLine(i);
+Console.WriteLine(outputarray.Length);
+//int sum=0;
+//foreach (int number in outputarray)
+//	sum+=number;
+//Console.WriteLine(sum);
